8b510c66dee4f1ba7fed9fc5f1a3c8bab2b588fe
Arpit7979 <arpitbuxar79@gmail.com>
2024-10-29 12:48:41 +0530

    connect to mongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,40 @@
+//first approach to connect to the database
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+dotenv.config({path:'./.env'});
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+
+// (async()=>{
+//     try {
+//       await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+//       app.on("error",(error)=>{
+//         console.log("error :", error);
+//         throw error;
+//       });
+
+//       app.listen(process.env.PORT,()=>{
+//         console.log(`Server is running on port ${process.env.PORT}`);
+//       });
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     }
+// })()
